b76d6d462c96984fcfcda23a018707628485ca69
鈴木康文 <uni0360@gmail.com>
2018-03-21 18:20:06 +0900

    modelの作成

---
+++ models/user.go
@@ -0,0 +1,51 @@
+package models
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+)
+
+var engine *xorm.Engine
+
+// init ...
+func init() {
+	var err error
+	engine, err = xorm.NewEngine("mysql", "fuga:pass@/bookshelf")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// User is
+type User struct {
+	ID       int    `json:"id" xorm:"'id'"`
+	UserName string `json:"name" xorm:"'nickname'"`
+}
+
+// NewUser ...
+func NewUser(id int, username string) User {
+	return User{
+		ID:       id,
+		UserName: username,
+	}
+}
+
+// UserRepository is
+type UserRepository struct {
+}
+
+// NewUserRepository ...
+func NewUserRepository() UserRepository {
+	return UserRepository{}
+}
+
+// GetByID ...
+func (m UserRepository) GetByID(id int) *User {
+	var user = User{ID: id}
+	has, _ := engine.Get(&user)
+	if has {
+		return &user
+	}
+
+	return
+}
